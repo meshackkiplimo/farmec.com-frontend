3d6898d34262fa5b2ef05d47d057e00405b6e920
Hezzie <hezronchelimo.hc@gmail.com>
2024-06-14 21:22:06 +0300

    -Added about page (#5)
    - Added services page

--- src/components/MainNav.tsx
+++ src/components/MainNav.tsx
@@ -1,25 +1,36 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import { Button } from "./ui/button";
 import UsernameMenu from "./UsernameMenu";
+import { NavLink } from "react-router-dom";
 
 const MainNav = () => {
-    const {loginWithRedirect, isAuthenticated}= useAuth0();
-  return(
+  const { loginWithRedirect, isAuthenticated } = useAuth0();
+  return (
     <span className="flex-space-x-2 items-center">
 
-        {isAuthenticated ? (
-         <UsernameMenu />):(
-         <Button 
-        variant="ghost" 
-        className="font-bold hover:text-green-500 hover:bg-white"
-        onClick={async() => await loginWithRedirect()}
-    >
-        Log in
-        
-    </Button>  
-    )}
-    </span>
-    
+      {isAuthenticated ? (
+        <div className="flex flex-row">
+          <div className="mr-4">
+            <NavLink to="/services" className="text-blue-500 hover:text-blue-700 px-4 py-2 rounded-md bg-gray-200 hover:bg-gray-300">Services</NavLink>
+          </div>
+          <div className="mr-4">
+            <NavLink to="/about" className="text-blue-500 hover:text-blue-700 px-4 py-2 rounded-md bg-gray-200 hover:bg-gray-300">About</NavLink>
+          </div>
+          <UsernameMenu />
+        </div>
+      ) : (
+        <Button
+          variant="ghost"
+          className="font-bold hover:text-green-500 hover:bg-white"
+          onClick={async () => await loginWithRedirect()}
+        >
+          Log in
+
+        </Button>
+      )
+      }
+    </span >
+
   )
 }
 
---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,8 @@
+
+const ServicesPage = () => {
+  return (
+    <h1>Coming Soon..!</h1>
+  )
+}
+
+export default ServicesPage;
