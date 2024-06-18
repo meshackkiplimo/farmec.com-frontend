a902a9bc5be6e95f87f8ac34454068f9f021c103
meshack <meshackkimaiyo5@gmail.com>
2024-06-18 21:06:49 +0300

    searchfilter

--- src/api/RentApi.tsx
+++ src/api/RentApi.tsx
@@ -1,17 +1,21 @@
+import { SearchState } from "@/pages/SearchPage"
 import { RentSearchResponse } from "@/types"
 import { useQuery } from "react-query"
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
 
-export const useSearchRents = (city?: string) =>{
+export const useSearchRents = (searchState: SearchState, city?: string) =>{
     const createSearchRequest = async ():Promise<RentSearchResponse> =>{
-        const response = await fetch (`${API_BASE_URL}/api/rent/search/${city}`)
+        const params = new URLSearchParams();
+        params.set("searchQuery", searchState. searchQuery)
+        const response = await fetch (`${API_BASE_URL}/api/rent/search/${city}?${params.toString()}`)
         if(!response.ok){
             throw new Error("Failed to get rent")
         }
         return response.json()
     }
-    const {data:results, isLoading} = useQuery(["search rents",city],
+    const {data:results, isLoading} = useQuery(
+        ["searchRents",searchState],
 
         createSearchRequest,
         {enabled: !!city}
