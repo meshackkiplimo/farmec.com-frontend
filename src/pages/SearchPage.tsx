3b0f9625fadd00537ca6850cb4a2737233227259
meshack <meshackkimaiyo5@gmail.com>
2024-06-18 22:09:57 +0300

    pagination

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,4 +1,5 @@
 import { useSearchRents } from "@/api/RentApi";
+import PaginationSelector from "@/components/PaginationSelector";
 import SearchBar, { SearchForm } from "@/components/SearchBar";
 import SearchResultCard from "@/components/SearchResultCard";
 import SearchResultInfor from "@/components/SearchResultInfor";
@@ -7,20 +8,31 @@ import { useParams } from "react-router-dom";
 
 export type SearchState = {
   searchQuery: string;
+  page:number;
 
 }
 
 const SearchPage = () => {
   const {city} = useParams()
   const [searchState,setSearchState] = useState<SearchState>({
-    searchQuery:  ""
+    searchQuery:  "",
+    page:1,
   })
   const {results, isLoading} = useSearchRents(searchState,city)
+
+  const setPage = (page:number) =>{
+    setSearchState((prevState)=>({
+      
+      ...prevState,page}))
+    
+
+  }
 
   const setSearchQuery = (searchFormData:SearchForm) =>{
     setSearchState((prevState)=> ({
       ...prevState,
       searchQuery: searchFormData.searchQuery,
+      page:1,
 
 
     }));
@@ -30,6 +42,7 @@ const SearchPage = () => {
       setSearchState((prevState)=> ({
         ...prevState,
         searchQuery: "",
+        page:1,
   
   
       }));
@@ -66,6 +79,12 @@ const SearchPage = () => {
         {results.data.map((rent)=>(
         <SearchResultCard rent={rent} />
         ))}
+        <PaginationSelector 
+         page={results.pagination.page}
+         pages={results.pagination.pages} 
+         onPageChange={setPage}
+        
+        />
     </div>
   </div>
   )  
