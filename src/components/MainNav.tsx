aa3287f8bb2361f9c0bd700ab9dc7a7bcfe21799
meshack <meshackkimaiyo5@gmail.com>
2024-06-29 16:24:33 +0300

    order status

--- src/components/MainNav.tsx
+++ src/components/MainNav.tsx
@@ -1,37 +1,31 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import { Button } from "./ui/button";
 import UsernameMenu from "./UsernameMenu";
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const MainNav = () => {
   const { loginWithRedirect, isAuthenticated } = useAuth0();
-  return (
-    <span className="flex-space-x-2 items-center">
 
+  return (
+    <span className="flex space-x-2 items-center">
       {isAuthenticated ? (
-        <div className="flex flex-row">
-          <div className="mr-4">
-            <NavLink to="/services" className="text-blue-500 hover:text-blue-700 px-4 py-2 rounded-md bg-gray-200 hover:bg-gray-300">Services</NavLink>
-          </div>
-          <div className="mr-4">
-            <NavLink to="/about" className="text-blue-500 hover:text-blue-700 px-4 py-2 rounded-md bg-gray-200 hover:bg-gray-300">About</NavLink>
-          </div>
+        <>
+          <Link to="/order-status" className="font-bold hover:text-green-500">
+            Order Status
+          </Link>
           <UsernameMenu />
-        </div>
+        </>
       ) : (
         <Button
           variant="ghost"
           className="font-bold hover:text-green-500 hover:bg-white"
           onClick={async () => await loginWithRedirect()}
         >
-          Log in
-
+          Log In
         </Button>
-      )
-      }
-    </span >
-
-  )
-}
+      )}
+    </span>
+  );
+};
 
 export default MainNav;
--- src/pages/ManageRentPage.tsx
+++ src/pages/ManageRentPage.tsx
@@ -1,18 +1,43 @@
-import { useCreateMyRent, useGetMyRent } from "@/api/MyRentApi";
+import { useCreateMyRent, useGetMyRent, useGetMyRentOrders } from "@/api/MyRentApi";
 import ManageRentForm from "@/forms/manage-rent-form/ManageRentForm";
 import { useUpdateMyRent } from './../api/MyRentApi';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import OrderItemCard from "@/components/OrderItemCard";
 
 const ManageRentPage = () => {
   const {createRent,isLoading:isCreateLoading} = useCreateMyRent()
   const {rent} = useGetMyRent()
   const {updateRent,isLoading:isUpdateLoading} = useUpdateMyRent()
+
+  const { orders } = useGetMyRentOrders();
+
+
   const isEditing  = !!rent
   return(
-    <ManageRentForm 
-     rent={rent}
-     onSave={isEditing ? updateRent:createRent}
-       isLoading={isCreateLoading || isUpdateLoading}
+    <Tabs defaultValue="orders">
+    <TabsList>
+      <TabsTrigger value="orders">Orders</TabsTrigger>
+      <TabsTrigger value="manage-rent">Manage Rent</TabsTrigger>
+    </TabsList>
+    <TabsContent
+      value="orders"
+      className="space-y-5 bg-gray-50 p-10 rounded-lg"
+    >
+      <h2 className="text-2xl font-bold">{orders?.length} active orders</h2>
+      {orders?.map((order) => (
+        <OrderItemCard order={order} />
+      ))}
+    </TabsContent>
+    <TabsContent value="manage-restaurant">
+      <ManageRentForm
+        rent={rent}
+        onSave={isEditing ? updateRent : createRent}
+        isLoading={isCreateLoading || isUpdateLoading}
       />
+    </TabsContent>
+  </Tabs>
+
+   
   )
 }
 
