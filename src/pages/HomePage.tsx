b33935711946595378068426d33651d254d55208
meshack <meshackkimaiyo5@gmail.com>
2024-07-05 07:32:48 +0300

    removed some pictures

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,5 @@
-import landingImage from "../assets/landing.png"
-import appDownloadImage from "../assets/appDownload.jpeg"
+// import landingImage from "../assets/landing.png"
+// import appDownloadImage from "../assets/appDownload.jpeg"
 import SearchBar, { SearchForm } from "@/components/SearchBar"
 import { useNavigate } from "react-router-dom"
 // import heroImage from "../assets/hero.png"
@@ -22,17 +22,17 @@ const HomePage = () => {
 
         <div className="md:px-32 bg-white rounded-lg shadow-md py-8 flex flex-col gap-5 text-center -mt-16">
            <h1 className="text-5xl font-bold tracking-tight text-green-600" > Machines for your choice</h1>
-           <span className="text-xl" >make your order today and you will never regret</span>
+           <span className="text-xl" >Rent your machine today</span>
 
         </div>
          <SearchBar placeHolder="search by city or town" onsubmit={handleSearchSubmit}/>
          <div className="grid md:grid-cols-2 gap-5">
-          <img src={landingImage} />
+          {/* <img src={landingImage} /> */}
           <div className="flex flex-col items-center justify-center gap-4 text-center" >
 
             <span className="font-bold text-3xl tracking-tighter">Book your favorite machine anytime</span>
             <span>We have a variety of machines that suites all farmers</span>
-            <img src={appDownloadImage} />
+            {/* <img src={appDownloadImage} /> */}
           </div>
 
          </div>
